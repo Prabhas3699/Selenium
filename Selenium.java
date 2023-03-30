7ab36206d0d10f288d1f2634c0a2327a1650e67f
Prabhas3699 <prabhak620@gmail.com>
2023-03-30 13:46:25 +0530

    selenium basic

---
+++ Selenium.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.caq115selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Selenium {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+        ChromeOptions options =new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        driver.get("https://www.facebook.com/");
+
+        driver.findElement(By.cssSelector("#email")).sendKeys("8109555221");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("pass")).sendKeys("Test12345");
+        Thread.sleep(2000);
+
+        driver.findElement(By.tagName("button")).click();
+        Thread.sleep(10000);
+
+        driver.close();
+    }
+}
